f14ac13f97ba6563dadb0a4703c6a45acc3f61e1
Kshitij Aranke <kshitij.aranke@gmail.com>
2015-08-08 18:19:21 -0700

    added front-end goodness

--- twemoji.js
+++ twemoji.js
@@ -20,52 +20,45 @@ if (Meteor.isServer) {
 
     var stream = T.stream('statuses/sample');
 
-stream.on('tweet', Meteor.bindEnvironment (function (tweet) {
+	stream.on('tweet', Meteor.bindEnvironment (function (tweet) {
 
-  if(tweet['lang'] === 'en') {
-    if(tweet['place'] !== null) {
+	  if(tweet['lang'] === 'en') {
+	    if(tweet['place'] !== null) {
 
-      console.log(tweet['id'] + ', ' + tweet['place']['bounding_box']['coordinates'][0][0]+ ', ' + tweet['text']);
-
-
-      var id = tweet['id'];
-      var coords = tweet['place']['bounding_box']['coordinates'][0][0];
-      var enText = tweet['text'];
-      var sentiVal = sentiment(enText);
-      Tweets.insert({
-        id: id,
-        coordinates: coords,
-        en_text: enText,
-        senti: sentiVal
-      });
-    }
-  } else {
-    if(tweet['place'] !== null) {
-
-      console.log(tweet['id'] + ', ' + tweet['place']['bounding_box']['coordinates'][0][0] + ', ' + tweet['text']);
+	      console.log(tweet['id'] + ', ' + tweet['place']['bounding_box']['coordinates'][0][0]+ ', ' + tweet['text']);
 
 
-      var id = tweet['id'];
-      var coords = tweet['place']['bounding_box']['coordinates'][0][0];
-      var enText = Microsoft.translate(tweet['text'], "en");
-      var sentiVal = sentiment(enText);
-      Tweets.insert({
-        id: id,
-        coordinates: coords,
-        en_text: enText,
-        senti: sentiVal
-      });
-    }
-  }
-}));
+	      var id = tweet['id'];
+	      var coords = tweet['place']['bounding_box']['coordinates'][0][0];
+	      var enText = tweet['text'];
+	      var sentiVal = sentiment(enText);
+	      Tweets.insert({
+	        id: id,
+	        coordinates: coords,
+	        en_text: enText,
+	        senti: sentiVal
+	      });
+	    }
+	  } else {
+	    if(tweet['place'] !== null) {
+
+	      console.log(tweet['id'] + ', ' + tweet['place']['bounding_box']['coordinates'][0][0] + ', ' + tweet['text']);
 
 
-
+	      var id = tweet['id'];
+	      var coords = tweet['place']['bounding_box']['coordinates'][0][0];
+	      var enText = Microsoft.translate(tweet['text'], "en");
+	      var sentiVal = sentiment(enText);
+	      Tweets.insert({
+	        id: id,
+	        coordinates: coords,
+	        en_text: enText,
+	        senti: sentiVal
+	      });
+	    }
+	  }
+	}));
   });
-}
-
-//invoke the server method
-if (Meteor.isClient) {
 }
 
 Meteor.methods({
@@ -83,3 +76,72 @@ Meteor.methods({
     return value;
   }
 });
+
+if (Meteor.isClient) {
+  Template.vis.rendered = function () {
+    var width = 960,
+    height = 500;
+
+    var projection = d3.geo.orthographic()
+        .scale(248)
+        .clipAngle(90);
+
+    var canvas = d3.select("body").append("canvas")
+        .attr("width", width)
+        .attr("height", height);
+
+    var c = canvas.node().getContext("2d");
+
+    var path = d3.geo.path()
+        .projection(projection)
+        .context(c);
+
+    var title = d3.select("h1");
+
+    queue()
+        .defer(d3.json, "world-110m.json")
+        .defer(d3.tsv, "world-country-names.tsv")
+        .await(ready);
+
+    function ready(error, world, names) {
+      if (error) throw error;
+
+      var globe = {type: "Sphere"},
+          land = topojson.feature(world, world.objects.land),
+          countries = topojson.feature(world, world.objects.countries).features,
+          borders = topojson.mesh(world, world.objects.countries, function(a, b) { return a !== b; }),
+          i = -1,
+          n = countries.length;
+
+      countries = countries.filter(function(d) {
+        return names.some(function(n) {
+          if (d.id == n.id) return d.name = n.name;
+        });
+      }).sort(function(a, b) {
+        return a.name.localeCompare(b.name);
+      });
+
+      (function transition() {
+        d3.transition()
+            .duration(1250)
+            .each("start", function() {
+              title.text(countries[i = (i + 1) % n].name);
+            })
+            .tween("rotate", function() {
+              var p = d3.geo.centroid(countries[i]),
+                  r = d3.interpolate(projection.rotate(), [-p[0], -p[1]]);
+              return function(t) {
+                projection.rotate(r(t));
+                c.clearRect(0, 0, width, height);
+                c.fillStyle = "#fee6ce", c.beginPath(), path(land), c.fill();
+                c.fillStyle = "#e6550d", c.beginPath(), path(countries[i]), c.fill();
+                c.strokeStyle = "#fff", c.lineWidth = .5, c.beginPath(), path(borders), c.stroke();
+                c.strokeStyle = "#fff", c.lineWidth = 2, c.beginPath(), path(globe), c.stroke();
+              };
+            })
+          .transition()
+            .each("end", transition);
+      })();
+    }
+  };
+}
